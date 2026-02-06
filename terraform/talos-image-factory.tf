fef5b8c97cbd25d1b15ec68788e10d885c3871d2
XuhuiSun95 <ericsun1995@gmail.com>
2026-02-06 10:12:28 -0800

    Add Talos image factory schematic for worker extensions
    This commit introduces a new data source for Talos image factory extensions versions and creates a schematic for worker images, incorporating the official QEMU guest agent extension. The schematic ID is now dynamically referenced in the Talos image factory URLs resource.

--- terraform/talos-image-factory.tf
+++ terraform/talos-image-factory.tf
@@ -11,12 +11,30 @@ locals {
   talos_image_version   = "${local.talos_platform}-${local.talos_architecture}-${local.talos_release_version}"
 }
 
+data "talos_image_factory_extensions_versions" "worker_extensions" {
+  talos_version = local.talos_release_version
+  filters = {
+    names = [
+      "qemu-guest-agent"
+    ]
+  }
+}
+
+resource "talos_image_factory_schematic" "worker_image_factory_schematic" {
+  schematic = yamlencode(
+    {
+      customization = {
+        systemExtensions = {
+          officialExtensions = data.talos_image_factory_extensions_versions.worker_extensions.extensions_info.*.name
+        }
+      }
+    }
+  )
+}
+
 data "talos_image_factory_urls" "talos_image" {
   talos_version = local.talos_release_version
-  # Hash: 376567988ad370138ad8b2698212367b8edcb69b5fd68c80be1f2ec7d603b4ba customization: {}
-  # Hash: 14e9b0100f05654bedf19b92313cdc224cbff52879193d24f3741f1da4a3cbb1 customization: siderolabs/binfmt-misc
-  # Hash: ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515 customization: siderolabs/qemu-guest-agent
-  schematic_id = "ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515"
-  architecture = local.talos_architecture
-  platform     = local.talos_platform
+  schematic_id  = talos_image_factory_schematic.worker_image_factory_schematic.id
+  architecture  = local.talos_architecture
+  platform      = local.talos_platform
 }
